57915eda4df7ed929d695729f0ad7da80f19cb20
yermaka <ermak.080@mail.ru>
2023-12-09 22:38:13 +0800

    feat: add button

--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -5,18 +5,47 @@ const burgerMenuVector = document.querySelector('.burger-menu__vector')
 
 window.addEventListener('resize', () => {
   if (window.screen.width > 768) {
-
+    back.removeEventListener('click', backEvent)
     burgerMenuVector.classList.remove('burger-menu__vector_click')
     burgerMenu.lastElementChild.classList.remove('burger-menu__vector_click-last')
 
     back.classList.remove('navbar__right')
     back.classList.remove('navbar__click')
+  } else {
+    back.removeEventListener('click', backEvent)
+    back.addEventListener('click', backEvent)
   }
 })
 //Слушатель для управления бургер-меню
 burgerMenu.addEventListener('click', changeBurgerMenu)
-//слушатель для скрытия выдвигающегося меню
-back.addEventListener('click', (e) => {
+//слушатель для скрытия  выдвигающимся меню
+back.addEventListener('click', backEvent)
+
+function changeBurgerMenu() {
+  burgerMenuVector.classList.toggle('burger-menu__vector_click')
+  burgerMenu.lastElementChild.classList.toggle('burger-menu__vector_click-last')
+  if (back.classList.contains('navbar__click') && back.classList.contains('navbar__right')) {
+    setTimeout(() => {
+      back.classList.toggle('navbar__right')
+    }, 50)
+    setTimeout(() => {
+
+      back.classList.toggle('navbar__click')
+    }, 500)
+  } else {
+    setTimeout(() => {
+
+      back.classList.toggle('navbar__click')
+    }, 50)
+    setTimeout(() => {
+
+      back.classList.toggle('navbar__right')
+    }, 250)
+  }
+}
+
+
+function backEvent(e) {
   e.preventDefault()
   if (!e.target.classList.contains('navbar') && !e.target.classList.contains('back')) {
     burgerMenuVector.classList.remove('burger-menu__vector_click')
@@ -41,30 +70,4 @@ back.addEventListener('click', (e) => {
       }
     }, 500)
   }
-})
-
-function changeBurgerMenu() {
-  burgerMenuVector.classList.toggle('burger-menu__vector_click')
-  burgerMenu.lastElementChild.classList.toggle('burger-menu__vector_click-last')
-  if (back.classList.contains('navbar__click') && back.classList.contains('navbar__right')) {
-    setTimeout(() => {
-      back.classList.toggle('navbar__right')
-    }, 50)
-    setTimeout(() => {
-
-      back.classList.toggle('navbar__click')
-    }, 500)
-  } else {
-    setTimeout(() => {
-
-      back.classList.toggle('navbar__click')
-    }, 50)
-    setTimeout(() => {
-
-      back.classList.toggle('navbar__right')
-    }, 250)
-  }
-
-
-
 }
