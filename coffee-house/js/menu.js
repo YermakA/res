74e4d76200dc966283a58328624072e4f921a81d
yermaka <ermak.080@mail.ru>
2023-12-09 13:58:06 +0800

    feat: burger-menu add smooth scrolling and smooth disappearance on click links

--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -13,11 +13,35 @@ window.addEventListener('resize', () => {
     back.classList.remove('navbar__click')
   }
 })
-
-
-
+//Слушатель для управления бургер-меню
 burgerMenu.addEventListener('click', changeBurgerMenu)
+//слушатель для скрытия выдвигающегося меню
+back.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (!e.target.classList.contains('navbar') && !e.target.classList.contains('back')) {
+    burgerMenuVector.classList.remove('burger-menu__vector_click')
+    burgerMenu.lastElementChild.classList.remove('burger-menu__vector_click-last')
+    if (back.classList.contains('navbar__click') && back.classList.contains('navbar__right')) {
+      setTimeout(() => {
+        back.classList.toggle('navbar__right')
+      }, 50)
+      setTimeout(() => {
+        back.classList.toggle('navbar__click')
+      }, 500)
+    }
 
+    setTimeout(() => {
+      if (e.target.classList.contains('coffee-menu__coffee-cup') || e.target.classList.contains('header__text') && e.target.classList.contains('coffee-menu__text')) {
+        document.location.href = e.target
+          .parentNode
+          .parentNode
+          .getAttribute('href')
+      } else {
+        document.location.href = e.target.parentNode.getAttribute('href')
+      }
+    }, 500)
+  }
+})
 
 function changeBurgerMenu() {
   burgerMenuVector.classList.toggle('burger-menu__vector_click')
