04d22534d5ed7944a7b58eeca85bc73ef66432bd
yermaka <ermak.080@mail.ru>
2024-01-02 23:42:03 +0800

    feat: media queries add for 1400 to 705px

--- rs-rep/js/index.js
+++ rs-rep/js/index.js
@@ -23,7 +23,7 @@ const answers = [
   "Hedgehog",
 ]
 //svg elements
-const svgGallows = `<svg width="500" height="581" viewBox="0 0 353 581" fill="none" xmlns="http://www.w3.org/2000/svg">
+const svgGallows = `<svg class="gallows" width="500" height="581" viewBox="0 0 353 581" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect x="176.337" y="34.6662" width="39" height="199.598" transform="rotate(45 176.337 34.6662)" fill="black" stroke="#FFFEFE" stroke-width="3"></rect>
 <rect x="34.5" y="1.5" width="39" height="578" rx="3.5" fill="black" stroke="#FFFEFE" stroke-width="3"></rect>
 <rect x="351.5" y="-40.5" width="39" height="450" rx="3.5" transform="rotate(90 351.5 34.5)" fill="black" stroke="#FFFEFE" stroke-width="3"></rect>
